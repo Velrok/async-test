3effe5ebe622fc73ef76418f91ba0d42c5a3c164
Waldemar <waldemar.schwan@adcloud.com>
2013-08-16 10:10:11 +0200

    drop dependency on core

--- spec/async_test/execution_strategie_spec.clj
+++ spec/async_test/execution_strategie_spec.clj
@@ -1,16 +1,15 @@
 (ns async-test.execution-strategie-spec
-  (:require [async-test.core :refer [slow-fn]]
-            [speclj.core :refer :all]
+  (:require [speclj.core :refer :all]
             [async-test.execution-strategie :refer :all]))
 
 (describe "test funtions"
   (with input (range 30))
   (it "returns the identity"
     (should= @input
-            (plain-apply slow-fn @input)))
+              (plain-apply identity @input)))
   (it "plain-apply equals pmap-apply"
-    (should= (plain-apply slow-fn @input)
-             (pmap-apply  slow-fn @input)))
+    (should= (plain-apply identity @input)
+             (pmap-apply  identity @input)))
   (it "plain-apply equals async-apply"
-    (should= (plain-apply slow-fn @input)
-             (async-apply slow-fn @input))))
+    (should= (plain-apply identity @input)
+             (async-apply identity @input))))
